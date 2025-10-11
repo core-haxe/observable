5b0101647bb1e6a152e36b9fbda84bcd29ae266e
Ian Harrigan <ianharrigan@hotmail.com>
2025-10-11 13:18:18 +0200

    use map

--- src/observable/Changes.hx
+++ src/observable/Changes.hx
@@ -1,18 +1,35 @@
 package observable;
 
 class Changes {
-    public var items:Array<ChangeInfo<Any>> = [];
-
     public function new() {
-
     }
 
-    public function contains(field:String) {
-        for (change in this.items) {
-            if (change.field == field) {
-                return true;
+    private var _items:Array<ChangeInfo<Any>> = [];
+    public var items(get, set):Array<ChangeInfo<Any>>;
+    private function get_items():Array<ChangeInfo<Any>> {
+        return _items;
+    }
+    private function set_items(value:Array<ChangeInfo<Any>>):Array<ChangeInfo<Any>> {
+        _items = value;
+        buildItemsMap();
+        return value;
+    }
+
+    private var _itemsMap:Map<String, Array<ChangeInfo<Any>>> = [];
+    private function buildItemsMap() {
+        _itemsMap.clear();
+        for (item in _items) {
+            var list = _itemsMap.get(item.field);
+            if (list == null) {
+                list = [];
+                _itemsMap.set(item.field, list);
             }
+            list.push(item);
         }
-        return false;
+    }
+
+
+    public function contains(field:String) {
+        return _itemsMap.exists(field);
     }
 }
